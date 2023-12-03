1ae7e782980c1ab5c57e2a21c733fda5b6843f54
flowa <152752280+flowa-ai@users.noreply.github.com>
2023-12-02 23:22:42 -0500

    Update _version.py
    Signed-off-by: flowa <152752280+flowa-ai@users.noreply.github.com>

--- flowa/_version.py
+++ flowa/_version.py
@@ -7,6 +7,10 @@ Module containing the version info of Flowa.
 PYTHON_REQUIRED_MAJOR = 3
 PYTHON_REQUIRED_MINOR = 7
 
+FLOWA_MAJOR = 1
+FLOWA_MINOR = 2
+FLOWA_PATCH = 0
+
 
 def get_python(*args, **kwargs) -> tuple:
     return (PYTHON_REQUIRED_MAJOR, PYTHON_REQUIRED_MINOR)
